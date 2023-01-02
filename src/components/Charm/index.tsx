91b549accf2b1af8434077f34d7e9ff32fed3c8b
Zak Strassberg <zakstrassberg@gmail.com>
2023-01-01 21:43:12 -0800

    style

--- src/components/Charm/index.tsx
+++ src/components/Charm/index.tsx
@@ -4,37 +4,34 @@ import { Link } from "react-router-dom";
 import { linkedComponent } from "../../package";
 import CharmClass from "../../shapes/CharmClass";
 
-const Charm = linkedComponent<CharmClass>(
-  CharmClass,
-  ({ source }) => {
-    return (
-      <article>
-        <header>
-          <h1>{source.name}</h1>
-          {source.charmset.isInCharmset && (
-            <Link to={source.charmset.uri}>
-              {source.charmset.charmset?.getValue(rdfs.label)}
-            </Link>
-          )}
-        </header>
-        <dl>
-          <dt>Cost</dt>
-          <dd>{source.moteCost}m</dd>
-          <dt>Mins</dt>
-          <dd>{source.requirements}</dd>
-          <dt>Type</dt>
-          <dd>{source.charmType}</dd>
-          <dt>Keywords</dt>
-          <dd>{source.charmKeywords}</dd>
-          <dt>Duration</dt>
-          <dd>{source.charmDuration}</dd>
-          <dt>Prerequisite Charms</dt>
-          <dd>{source.charmPrerequisites}</dd>
-        </dl>
-        <p>{source.description}</p>
-      </article>
-    );
-  }
-);
+const Charm = linkedComponent<CharmClass>(CharmClass, ({ source }) => {
+  return (
+    <article>
+      <header>
+        <h1>{source.name}</h1>
+        {source.charmset.isInCharmset && (
+          <Link to={source.charmset.uri}>
+            {source.charmset.charmset?.getValue(rdfs.label)}
+          </Link>
+        )}
+      </header>
+      <dl style={{ display: "grid", gridTemplateColumns: "max-content auto" }}>
+        <dt>Cost</dt>
+        <dd>{source.moteCost}m</dd>
+        <dt>Mins</dt>
+        <dd>{source.requirements}</dd>
+        <dt>Type</dt>
+        <dd>{source.charmType}</dd>
+        <dt>Keywords</dt>
+        <dd>{source.charmKeywords}</dd>
+        <dt>Duration</dt>
+        <dd>{source.charmDuration}</dd>
+        <dt>Prerequisite Charms</dt>
+        <dd>{source.charmPrerequisites}</dd>
+      </dl>
+      <p>{source.description}</p>
+    </article>
+  );
+});
 
 export default Charm;
